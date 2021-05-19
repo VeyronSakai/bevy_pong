d0056e17c0960c0eed4373f83fd45a410eaef8b0
VeyronSakai <yuukisakai123@gmail.com>
2021-05-20 05:10:14 +0900

    長方形に変形させる

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,12 @@ fn main() {
         // 初期化処理。StartUp Stageで実行される。
         .add_startup_system(setup.system())
         .add_startup_stage("game_setup", SystemStage::single(spawn_paddle.system()))
+        // PostUpdateで実行したいので、add_system_setではなく、add_system_set_to_stageを使う
+        .add_system_set_to_stage(
+            CoreStage::PostUpdate,
+            SystemSet::new()
+                .with_system(size_scaling.system()),
+        )
         .run();
 }
 
@@ -38,7 +44,31 @@ pub fn spawn_paddle(
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.paddle_body_material.clone(),
-            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
             ..Default::default()
-        });
+        })
+        .insert(SpriteSize::new(0.3, 1.));
+}
+
+fn size_scaling(windows: Res<Windows>, mut q: Query<(&SpriteSize, &mut Sprite)>) {
+    let window = windows.get_primary().unwrap();
+    for (sprite_size, mut sprite) in q.iter_mut() {
+        sprite.size = Vec2::new(
+            sprite_size.width / 10 as f32 * window.width() as f32,
+            sprite_size.height / 10 as f32 * window.height() as f32,
+        );
+    }
+}
+
+pub struct SpriteSize {
+    pub width: f32,
+    pub height: f32,
+}
+
+impl SpriteSize {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self {
+            width: x.clone(),
+            height: y.clone(),
+        }
+    }
 }
