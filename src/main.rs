d5bb8302c437d72f09b6306d58cc02ee95290428
VeyronSakai <yuukisakai123@gmail.com>
2021-05-20 04:55:59 +0900

    とりあえず四角形を表示するようにする

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,33 @@ fn main() {
         .add_plugins(DefaultPlugins)
         // 初期化処理。StartUp Stageで実行される。
         .add_startup_system(setup.system())
+        .add_startup_stage("game_setup", SystemStage::single(spawn_paddle.system()))
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     // カメラを生成する
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    // Material
+    commands.insert_resource(Materials {
+        paddle_body_material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
+    });
     // 背景の色を黒くする
     commands.insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)));
+}
+
+pub struct Materials {
+    pub paddle_body_material: Handle<ColorMaterial>,
+}
+
+pub fn spawn_paddle(
+    mut commands: Commands,
+    materials: Res<Materials>,
+) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.paddle_body_material.clone(),
+            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
+            ..Default::default()
+        });
 }
